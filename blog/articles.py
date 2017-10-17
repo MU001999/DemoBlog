79dd0f4f98ac37459c55d3db6ed566a72d640384
mu001999 <mu001999@outlook.com>
2017-10-17 19:52:32 +0800

    add half article

--- blog/articles.py
+++ blog/articles.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 from flask import render_template, redirect, request, session
 from blog import app
 from blog.link2db import *
@@ -15,11 +16,17 @@ def paste_article():
         except:
             return redirect('/login')
     title, author, content = request.form['title'], session['nickname'], request.form['content']
-    post_id = add_article(title, author, content)
+    time_post = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    post_id = add_article(title, author, content, time_post)
     return redirect('/articles/' + str(post_id))
 
 
 @app.route('/articles/<int:article_id>', methods=['GET', 'POST'])
 def set_article(article_id):
-    title, author, content = get_article(article_id)
-    return render_template('article.html', title=title, author=author, content=content)
+    title, author, content, post_time = get_article(article_id)
+    return render_template('article.html', title=title, author=author, content=content, post_time=post_time)
+
+
+@app.route('/articles/all/<int:page_id>')
+def set_articles(page_id):
+    page_ids = range(max(0, page_id-2), page_id)
