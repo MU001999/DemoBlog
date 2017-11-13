91b8feae8d41e87be95983d501c53bc6085e998a
mu001999 <mu001999@outlook.com>
2017-11-13 16:44:15 +0800

    edit the default port

--- runserver.py
+++ runserver.py
@@ -10,7 +10,7 @@ from blog import app, socketio
 if __name__ == "__main__":
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_HOST', '5555'))
+        PORT = int(environ.get('SERVER_HOST', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     socketio.run(app, debug=True, host=HOST, port=PORT)
